c4dc48a58267b3aaa26c6a9aa0eca6828588afd6
JudeA94 <ashworthjude@gmail.com>
2023-01-25 17:50:13 +0000

    game logic tested

--- src/Helpers/GameOfLife.test.js
+++ src/Helpers/GameOfLife.test.js
@@ -5,8 +5,8 @@ const GameOfLife = require("./gameOfLife")
 describe('GameOfLife', () => {
   const grid = [[1,1,1],[1,0,0],[0,1,0]]
   const paddedGrid = [[0,0,0,0,0],[0,1,1,1,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,0,0]]
-  const nextGrid = [ [ 1, 1, 1 ], [ 1, 0, 0 ], [ 0, 1, 0 ] ]
-  const nextNextGrid = [ [ 1, 1, 1 ], [ 1, 0, 0 ], [ 0, 1, 0 ] ]
+  const nextGrid = [ [ 1, 1, 0 ], [ 1, 0, 1 ], [ 0, 0, 0 ] ]
+  const nextNextGrid = [ [ 1, 1, 0 ], [ 1, 0, 0 ], [ 0, 0, 0 ] ]
   it('can add padding of 0s to a given grid', () => {
     const gameOfLife = new GameOfLife()
     expect(gameOfLife.addPadding(grid)).toEqual(paddedGrid)
@@ -15,5 +15,15 @@ describe('GameOfLife', () => {
     const gameOfLife = new GameOfLife()
     expect(gameOfLife.getNeighbours(paddedGrid,1,1)).toEqual([0,0,0,0,1,0,1,0])
   })
-
+    it('can get the next grid in the game', () => {
+    const gameOfLife = new GameOfLife()
+    expect(gameOfLife.nextGrid(grid)).toEqual(nextGrid)
+  })
+  it('can be played muultiple times', () => {
+    const gameOfLife = new GameOfLife()
+    const next = gameOfLife.nextGrid(grid)
+    console.log(nextNextGrid)
+    console.log(gameOfLife.nextGrid(next))
+    expect(gameOfLife.nextGrid(next)).toEqual(nextNextGrid)
+  })
 })
